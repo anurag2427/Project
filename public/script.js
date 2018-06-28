4bc07e56efb4a660095508ceb96d6a56fe23c99b
anurag2427 <anurag424.at@gmail.com>
2018-06-29 01:31:18 +0530

    yes

---
+++ public/script.js
@@ -0,0 +1,8 @@
+function aboutUs()
+{
+	document.getElementById('a').innerHTML='<center><h3>URL SHORTNER</h3><br><div>A URL shortener is a Web site that will create a short Uniform Resource Locator (URL) or Web page address from a long one so that the short version, which is easier to remember and enter, can be used instead. For example, if you need to frequently enter a URL into e-mail messages or read it over the phone, a long URL is prone to mistyping or misunderstanding. By going to a free Web site service like TinyURL.com or notlong.com, you can swap your long URL for a short one. The URL shortening service will maintain the association between the two and will redirect anyone who enters the short URL to the page at the long URL.</div></center><div class="footer" style="width:500px"><center><p>&copy;ANURAG , KARTIK MEHMI , JATINDER KUMAR</p></center></div>';
+	document.getElementById('a').style.height="400px";
+    document.getElementById('a').style.width="300px";
+    document.getElementById('a').style.margin="auto";
+    document.getElementById('a').style.color="red";
+}
